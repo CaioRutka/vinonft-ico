8ec37587af3c44672031a674595109794ec5c5fd
CaioRutka <caio_rutka@hotmail.com>
2023-07-20 11:09:04 -0300

    first

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useState, Suspense } from 'react';
 import i18n from '../../i18n';
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+import { useTranslation } from "react-i18next";
 
 import NavBar from '../../components/NavBar';
 import Footer from '../../components/Footer';
@@ -17,10 +19,13 @@ import Loading from '../../components/Loading';
 import LocaleContext from '../../LocaleContext';
 
 import './index.css';
+import Alexandre from "../../assets/images/alexandre.jpg";
 
 function Home() {
   const [accounts, setAccounts] = useState([]);
   const [locale, setLocale] = useState(i18n.language);
+
+  const { t } = useTranslation();
 
   i18n.on('languageChanged', (lng) => setLocale(i18n.language));
 
@@ -50,34 +55,45 @@ function Home() {
 
   return (   
     <LocaleContext.Provider value={{locale, setLocale}}>
-    <Suspense fallback={<Loading />}>
-    <div className="overlay">
-      <div className="overlay">
-        <NavBar accounts = { accounts } setAccounts = {setAccounts} handleClickScrollAbout = {handleClickScrollAbout} handleClickScrollRoad = {handleClickScrollRoad}/>
-        <div id="swap-section">
-          <Swap accounts = { accounts } setAccounts = {setAccounts} handleClickScroll = {handleClickScroll}/>
-        </div>
-        
-        <div id="about-section">
-          <About handleClickScroll = {handleClickScroll}/>      
-        </div>
+      <Suspense fallback={<Loading />}>
+        <FloatingWhatsApp
+          phoneNumber = "+55 41 9517-5019"
+          accountName = "Alexandre Benites - CEO"
+          chatMessage = {t('whatsMessage')}
+          avatar = {Alexandre}
+          allowEsc
+          allowClickAway
+          notification
+          notificationSound
+          buttonStyle = {{ width: 50, height: 50 }}
+        />
+        <div className="overlay">
+          <div className="overlay">   
+            <NavBar accounts = { accounts } setAccounts = {setAccounts} handleClickScrollAbout = {handleClickScrollAbout} handleClickScrollRoad = {handleClickScrollRoad}/>
+            <div id="swap-section">
+              <Swap accounts = { accounts } setAccounts = {setAccounts} handleClickScroll = {handleClickScroll}/>
+            </div>
 
-        <Ecosystem />
-        <Vinocoin />
-        <ICOBox />
-        <ICOAbout handleClickScroll = {handleClickScroll}/>
-        <Tokenomics handleClickScroll = {handleClickScroll}/>
-        <div id="road-section">
-          <Roadmap/>             
-        </div>   
-        <Contact />
-        <Footer/>
-      </div>
-      <div className="moving-background">
-        <div className='opac-background'></div>
-      </div>
-    </div>
-    </Suspense>
+            <div id="about-section">
+              <About handleClickScroll = {handleClickScroll}/>      
+            </div>
+
+            <Ecosystem />
+            <Vinocoin />
+            <ICOBox />
+            <ICOAbout handleClickScroll = {handleClickScroll}/>
+            <Tokenomics handleClickScroll = {handleClickScroll}/>
+            <div id="road-section">
+              <Roadmap/>             
+            </div>   
+            <Contact />
+            <Footer/>
+          </div>
+          <div className="moving-background">
+            <div className='opac-background'></div>
+          </div>
+        </div>
+      </Suspense>
     </LocaleContext.Provider> 
   );
 }
